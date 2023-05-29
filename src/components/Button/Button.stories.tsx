be423dfd5b62015ea429ab915688cdcf3b8353c9
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-29 22:53:32 +0300

    add: storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Button from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  children: "Button",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: "Button",
+  disabled: true,
+};
