3830c0fa8decd910a20f78bf6e2f4650fa0d0aa9
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-05-29 23:53:33 +0300

    add: custom design

---
+++ .storybook/YourTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "mertsabinov-ui",
+  brandUrl: "/",
+  brandImage: "",
+  brandTarget: "_self",
+});
