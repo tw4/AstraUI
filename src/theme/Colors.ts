f21007297664d8e09553d4c1985021fe023be657
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-06-01 09:07:09 +0300

    add: default colors

---
+++ src/theme/Colors.ts
@@ -0,0 +1,50 @@
+import { ColorScheme } from "../types";
+import { DefaultColorValue } from "./theme";
+
+export const ThemePrimaryColor: ColorScheme = {
+  default: "#1846C7",
+  hover: "#1640B6",
+  focus: "#143AA4",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
+
+export const PurpleColorScheme: ColorScheme = {
+  default: DefaultColorValue.purple,
+  hover: "#7500A5",
+  focus: "#560087",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
+
+export const GreenColorScheme: ColorScheme = {
+  default: DefaultColorValue.green,
+  hover: "#15780C",
+  focus: "#0C4F08",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
+
+export const RedColorScheme: ColorScheme = {
+  default: DefaultColorValue.red,
+  hover: "#A60000",
+  focus: "#7A0000",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
+
+export const OrangeColorScheme: ColorScheme = {
+  default: DefaultColorValue.orange,
+  hover: "#D65200",
+  focus: "#934000",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
+
+export const YellowColorScheme: ColorScheme = {
+  default: DefaultColorValue.yellow,
+  hover: "#C28A00",
+  focus: "#825B00",
+  textLight: "#FFFFFF",
+  textDark: "#000000",
+};
