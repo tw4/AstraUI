629b46e5f06f19662a29561141c34e8ad7eb5e98
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-06-01 09:06:53 +0300

    add: color scheme

--- src/components/Button/Button-styles.ts
+++ src/components/Button/Button-styles.ts
@@ -1,88 +1,109 @@
-import { ThemePrimaryColor } from "../../theme/theme";
+import {
+  GreenColorScheme,
+  OrangeColorScheme,
+  PurpleColorScheme,
+  RedColorScheme,
+  ThemePrimaryColor,
+  YellowColorScheme,
+} from "../../theme/Colors";
 import { ButtonProps, ButtonVariant } from "./Button-types";
 import { FontSizeValue, SizeKey, SizeValue } from "../../theme/theme-enum";
 import styled, { css } from "styled-components";
+import { ColorScheme } from "../../types";
+import { DefaultColorKey } from "../../theme/theme";
 
-const variants = {
-  primary: css`
-    background-color: ${ThemePrimaryColor.default};
-    color: ${ThemePrimaryColor.textLight};
-    &:hover {
-      background-color: ${ThemePrimaryColor.hover};
-    }
-    &:focus {
-      background-color: ${ThemePrimaryColor.focus};
-    }
-    &:disabled {
-      cursor: default;
-      outline-style: none;
-      background-color: ${ThemePrimaryColor.default};
-      color: ${ThemePrimaryColor.textLight};
-      opacity: 0.5;
-    }
-  `,
-  ghost: css`
-    background-color: transparent;
-    border: 2px solid ${ThemePrimaryColor.default};
-    color: ${ThemePrimaryColor.default};
-    &:hover {
-      border-color: ${ThemePrimaryColor.hover};
-      color: ${ThemePrimaryColor.hover};
-    }
-    &:focus {
-      border-color: ${ThemePrimaryColor.focus};
-      color: ${ThemePrimaryColor.textLight};
-      background-color: ${ThemePrimaryColor.focus};
-    }
-    &:disabled {
-      cursor: default;
-      outline-style: none;
-      border-color: ${ThemePrimaryColor.default};
-      color: ${ThemePrimaryColor.default};
-      opacity: 0.5;
-    }
-  `,
-  text: css`
-    color: ${ThemePrimaryColor.default};
-    background-color: transparent;
-    border-bottom: 2px solid ${ThemePrimaryColor.hover};
-    border-radius: 0;
-    padding: 0 2px;
-    &:hover {
-      color: ${ThemePrimaryColor.hover};
-      border-color: ${ThemePrimaryColor.hover};
-    }
-    &:focus {
-      color: ${ThemePrimaryColor.textLight};
-      background-color: ${ThemePrimaryColor.focus};
-      border-color: ${ThemePrimaryColor.textLight};
-    }
-    &:disabled {
-      cursor: default;
-      color: ${ThemePrimaryColor.default};
-      border-color: ${ThemePrimaryColor.default};
-      opacity: 0.5;
-    }
-  `,
-  circle: css`
-    border-radius: 100%;
-    width: 50px;
-    height: 50px;
-    background-color: ${ThemePrimaryColor.default};
-    color: ${ThemePrimaryColor.textLight};
-    &:hover {
-      background-color: ${ThemePrimaryColor.hover};
-    }
-    &:focus {
-      background-color: ${ThemePrimaryColor.focus};
-    }
-    &:disabled {
-      cursor: default;
-      background-color: ${ThemePrimaryColor.default};
-      color: ${ThemePrimaryColor.textLight};
-      opacity: 0.5;
-    }
-  `,
+const variants = (color?: any) => {
+  let colorScheme: ColorScheme = ThemePrimaryColor;
+  switch (color) {
+    case DefaultColorKey.green:
+      colorScheme = GreenColorScheme;
+      break;
+    case DefaultColorKey.orange:
+      colorScheme = OrangeColorScheme;
+      break;
+    case DefaultColorKey.purple:
+      colorScheme = PurpleColorScheme;
+      break;
+    case DefaultColorKey.red:
+      colorScheme = RedColorScheme;
+      break;
+    case DefaultColorKey.yellow:
+      colorScheme = YellowColorScheme;
+      break;
+    default:
+      break;
+  }
+  return {
+    primary: css`
+      background-color: ${colorScheme.default};
+      color: ${colorScheme.textLight};
+      &:hover {
+        background-color: ${colorScheme.hover};
+      }
+      &:focus {
+        background-color: ${colorScheme.focus};
+      }
+      &:disabled {
+        outline-style: none;
+        background-color: ${colorScheme.default};
+        color: ${colorScheme.textLight};
+        opacity: 0.5;
+      }
+    `,
+    ghost: css`
+      background-color: transparent;
+      border: 2px solid ${colorScheme.default};
+      color: ${colorScheme.default};
+      &:hover {
+        border-color: ${colorScheme.hover};
+        color: ${colorScheme.hover};
+      }
+      &:focus {
+        border-color: ${colorScheme.focus};
+        color: ${colorScheme.textLight};
+        background-color: ${colorScheme.focus};
+      }
+      &:disabled {
+        cursor: default;
+        outline-style: none;
+        border-color: ${colorScheme.default};
+        color: ${colorScheme.default};
+        opacity: 0.5;
+      }
+    `,
+    text: css`
+      color: ${colorScheme.default};
+      background-color: transparent;
+      &:hover {
+        color: ${colorScheme.hover};
+      }
+      &:focus {
+        color: ${colorScheme.focus};
+      }
+      &:disabled {
+        color: ${colorScheme.default};
+        opacity: 0.5;
+      }
+    `,
+    circle: css`
+      border-radius: 100%;
+      width: 50px;
+      height: 50px;
+      background-color: ${colorScheme.default};
+      color: ${colorScheme.textLight};
+      &:hover {
+        background-color: ${colorScheme.hover};
+      }
+      &:focus {
+        background-color: ${colorScheme.focus};
+      }
+      &:disabled {
+        background-color: ${colorScheme.default};
+        color: ${colorScheme.textLight};
+        opacity: 0.5;
+      }
+    `,
+  };
 };
 
 export const StyledButton = styled.button<ButtonProps>`
@@ -107,19 +128,27 @@ export const StyledButton = styled.button<ButtonProps>`
       ? FontSizeValue.medium
       : props.size === SizeKey.large
       ? FontSizeValue.large
-      : FontSizeValue.small};
+      : FontSizeValue.medium};
   border-radius: 8px;
   border: none;
   width: ${(props) => (props.fullWidth ? "100%" : null)};
   ${(props) =>
     props.variant && props.variant === ButtonVariant.primary
-      ? variants.primary
+      ? props.colorScheme
+        ? variants(props.colorScheme).primary
+        : variants().primary
       : props.variant === ButtonVariant.ghost
-      ? variants.ghost
+      ? props.colorScheme
+        ? variants(props.colorScheme).ghost
+        : variants().ghost
       : props.variant === ButtonVariant.text
-      ? variants.text
+      ? props.colorScheme
+        ? variants(props.colorScheme).text
+        : variants().text
       : props.variant === ButtonVariant.circle
-      ? variants.circle
+      ? props.colorScheme
+        ? variants(props.colorScheme).circle
+        : variants().circle
       : null}
   &:hover {
     ${(props) => (props._hover ? props._hover : null)}
@@ -131,6 +160,7 @@ export const StyledButton = styled.button<ButtonProps>`
     ${(props) => (props._active ? props._active : null)}
   }
   &:disabled {
+    cursor: not-allowed;
     ${(props) => (props._disabled ? props._disabled : null)}
   }
 `;
