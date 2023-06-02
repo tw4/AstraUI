cd6420b105ddc32c49c10f1f046fc4e0a098fc0a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-06-02 12:05:13 +0300

    add: export Box

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Text } from "./Text";
 export { default as Input } from "./Input";
 export { default as Checkbox } from "./Checkbox";
+export { default as Box } from "./Box";
